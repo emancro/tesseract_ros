1ec188a6088d2ff0187c752fad5de97f3fdee6f6
Frederik Ebert <febert@emancro.ai>
2023-09-25 13:39:55 -0700

    added target transform

--- tesseract_ros_examples/src/xarm7_trajopt_node.cpp
+++ tesseract_ros_examples/src/xarm7_trajopt_node.cpp
@@ -58,6 +58,57 @@ void convert_deg_to_rad(Eigen::VectorXd& vec)
   }
 }
 
+void convert_rad_to_deg(Eigen::VectorXd& vec)
+{
+  for (int i = 0; i < vec.size(); i++)
+  {
+    vec(i) = vec(i) * 180.0 / M_PI;
+  }
+}
+
+Eigen::Quaterniond EulerToQuaternion(double roll, double pitch, double yaw) // roll (x), pitch (Y), yaw (z)
+{
+    // Abbreviations for the various angular functions
+
+    double cr = cos(roll * 0.5);
+    double sr = sin(roll * 0.5);
+    double cp = cos(pitch * 0.5);
+    double sp = sin(pitch * 0.5);
+    double cy = cos(yaw * 0.5);
+    double sy = sin(yaw * 0.5);
+
+    Eigen::Quaterniond q;
+    q.w() = cr * cp * cy + sr * sp * sy;
+    q.x() = sr * cp * cy - cr * sp * sy;
+    q.y() = cr * sp * cy + sr * cp * sy;
+    q.z() = cr * cp * sy - sr * sp * cy;
+
+    return q;
+}
+
+// this implementation assumes normalized quaternion
+// converts to Euler angles in 3-2-1 sequence
+Eigen::Vector3d ToEulerAngles(Eigen::Quaterniond q) {
+    Eigen::Vector3d angles;
+
+    // roll (x-axis rotation)
+    double sinr_cosp = 2 * (q.w() * q.x() + q.y() * q.z());
+    double cosr_cosp = 1 - 2 * (q.x() * q.x() + q.y() * q.y());
+    angles(0) = std::atan2(sinr_cosp, cosr_cosp);
+
+    // pitch (y-axis rotation)
+    double sinp = std::sqrt(1 + 2 * (q.w() * q.y() - q.x() * q.z()));
+    double cosp = std::sqrt(1 - 2 * (q.w() * q.y() - q.x() * q.z()));
+    angles(1) = 2 * std::atan2(sinp, cosp) - M_PI / 2;
+
+    // yaw (z-axis rotation)
+    double siny_cosp = 2 * (q.w() * q.z() + q.x() * q.y());
+    double cosy_cosp = 1 - 2 * (q.y() * q.y() + q.z() * q.z());
+    angles(2) = std::atan2(siny_cosp, cosy_cosp);
+
+    return angles;
+}
+
 
 
 int main(int argc, char** argv)
@@ -95,6 +146,36 @@ int main(int argc, char** argv)
   convert_deg_to_rad(arm_end_eig);  
 
 
+  std::vector<double> final_pose_euler;
+  pnh.param("final_pose_euler", final_pose_euler, final_pose_euler);
+  Eigen::VectorXd final_pose_euler_eig(final_pose_euler.size());
+  init_vectorXd(final_pose_euler_eig, final_pose_euler);
+  std::cout << "final_pose_euler_eig "<< final_pose_euler_eig << std::endl;
+  convert_deg_to_rad(final_pose_euler_eig);
+  std::cout << "final_pose_euler_eig rad "<< final_pose_euler_eig << std::endl;
+
+
+  std::vector<double> final_pose_xyz;
+  pnh.param("final_pose_xyz", final_pose_xyz, final_pose_xyz);
+  Eigen::Vector3d final_pose_xyz_eig(final_pose_xyz.data());
+
+  Eigen::Quaterniond quat_eig;
+  quat_eig = EulerToQuaternion(final_pose_euler_eig(0), final_pose_euler_eig(1), final_pose_euler_eig(2));
+  std::cout << "quat_eig vec " << quat_eig.x() << quat_eig.y() << quat_eig.z() << std::endl;
+  std::cout << "quat_eig w " << quat_eig.w() << std::endl;
+
+  Eigen::Vector3d euler_eig = ToEulerAngles(quat_eig);
+  std::cout << "convert back to euler "<< euler_eig(0)*180/M_PI << euler_eig(1)*180/M_PI << euler_eig(2)*180/M_PI << std::endl;
+  
+  Eigen::Isometry3d final_pose_eig;
+  final_pose_eig.linear() = quat_eig.matrix();
+  final_pose_eig.translation() = final_pose_xyz_eig;
+
+  
+  std::cout << "node final_pose_ rotation "<< final_pose_eig.linear() << std::endl;
+  std::cout << "node final_pose_ translation "<< final_pose_eig.translation() << std::endl;
+
+
   if (ifopt == true)
   {
     ROS_INFO("Using TrajOpt Ifopt!");
@@ -119,6 +200,6 @@ int main(int argc, char** argv)
   if (plotting)
     plotter = std::make_shared<ROSPlotting>(env->getSceneGraph()->getRoot());
 
-  Xarm7Trajopt example(env, plotter, ifopt, debug, sphere1_eigen, arm_start_eig, arm_end_eig);
+  Xarm7Trajopt example(env, plotter, ifopt, debug, sphere1_eigen, arm_start_eig, arm_end_eig, final_pose_eig);
   example.run();
 }
